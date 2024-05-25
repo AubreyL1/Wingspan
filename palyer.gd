bfe40592d306ade125cb5bde8329d0f4fa3875db
And E <the.hollowknight7850@gmail.com>
2024-05-25 15:56:32 -0400

    movement code

--- palyer.gd
+++ palyer.gd
@@ -1,10 +1,11 @@
 extends CharacterBody2D
 
 
-const SPEED = 20.0
 const JUMP_VELOCITY = -600.0
 
-var inertia = 0
+var lastDir = 0
+var speed = 10
+var dashing = false
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -12,28 +13,50 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
 	# Add the gravity.
-	if not is_on_floor():
+	if not is_on_floor()&&!dashing:
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+	if Input.is_action_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
-	inertia += SPEED
+	
 	if direction > 0:
-		velocity.x = inertia
+		#Moves right
+		if velocity.x > 0:
+			speed = 50
+		else:
+			speed = 20
+		velocity.x += speed
+		lastDir = direction
 	elif direction < 0:
-		velocity.x = -1 * inertia
+		#Moves left
+		if velocity.x > 0:
+			speed = 50
+		else:
+			speed = 20
+		velocity.x -= speed
+		lastDir = direction
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		inertia -= 50
-	#Limits speed
-	if inertia > 500:
-		inertia = 500
-	elif inertia < 0:
-		inertia = 0
+		#Stops the Bird when no movement keys are pressed
+		speed = move_toward(velocity.x, 0, 30)
+		velocity.x = speed
+		
+	#Limits speed of player
+	if velocity.x > 1000:
+		velocity.x -= 40
+	elif velocity.x < -1000:
+		velocity.x += 40
+	if velocity.x < 1000||velocity.x > -1000:
+		dashing = false
+		
+	# Handle Dash.
+	if Input.is_action_pressed("ui_accept"):
+		velocity.x = 2000*lastDir
+		dashing = true
+		velocity.y = 0
 		
 	move_and_slide()
