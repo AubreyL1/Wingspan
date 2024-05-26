d9e40e4c71387b25d91e8e2396931bcc0b8f5d03
And E <the.hollowknight7850@gmail.com>
2024-05-25 23:10:57 -0400

    Movement completed

--- palyer.gd
+++ palyer.gd
@@ -6,6 +6,7 @@ const JUMP_VELOCITY = -600.0
 var lastDir = 0
 var speed = 10
 var dashing = false
+var dashTime = 0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -26,12 +27,13 @@ func _physics_process(delta):
 	
 	if direction > 0:
 		#Moves right
-		if velocity.x > 0:
+		if velocity.x < 0:
 			speed = 50
 		else:
 			speed = 20
 		velocity.x += speed
 		lastDir = direction
+
 	elif direction < 0:
 		#Moves left
 		if velocity.x > 0:
@@ -50,13 +52,21 @@ func _physics_process(delta):
 		velocity.x -= 40
 	elif velocity.x < -1000:
 		velocity.x += 40
-	if velocity.x < 1000||velocity.x > -1000:
-		dashing = false
 		
-	# Handle Dash.
-	if Input.is_action_pressed("ui_accept"):
+	dash()
+	dashTime+=1
+	print(dashTime)
+	move_and_slide()
+
+# Handle Dash.
+func dash():
+	if (Input.is_action_just_pressed("ui_accept")&&dashTime > 60):
+		dashTime = 0
+		dashing = true
+	if (Input.is_action_just_pressed("ui_accept")&&dashTime > 60)||(dashTime < 15&&dashing):
 		velocity.x = 2000*lastDir
 		dashing = true
 		velocity.y = 0
-		
-	move_and_slide()
+	elif dashTime == 15:
+		dashing = false
+		velocity.x = 0
